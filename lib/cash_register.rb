f2b8f1225d0bdfcf51b628ad05c68c33fdcf3238
Alethea Cary <aletheacary@gmail.com>
2019-05-26 19:26:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+require "pry"
 
+class CashRegister
+  attr_accessor :discount, :total, :item, :price, :quantity
+  
+  @@items = []
+  
+  def initialize(discount = 0)
+    @discount = discount
+    @item = item
+    @price = price
+    @quantity = quantity
+    @total = 0
+  end
+  
+  def add_item(item, price, quantity = 1)
+    @item = item
+    @price = price
+    @quantity = quantity
+    @total = @total + (price * quantity)
+    @@items << self.new
+  end
+    
+  #def apply_discount
+   # @total.to_f*(1-(@discount.to_f*0.01))
+  #end
+end
